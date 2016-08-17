764c195dfcd516c288f6d6c0d52ac17bb5d535ae
Sven-Hendrik Haase <svenstaro@gmail.com>
2016-08-17 21:13:16 +0200

    Rotate swarm around player

--- src/systems/SwarmSystem.lua
+++ src/systems/SwarmSystem.lua
@@ -4,21 +4,27 @@ function SwarmSystem:update()
     for k, entity in pairs(self.targets) do
         --player_x, player_y = player:get('Physical').body:getPosition()
         --player_vector = Vector(player_x, player_y)
-        mother_x, mother_y = entity:get('SwarmMember').mothership:get('Physical').body:getPosition()
-        mother_vector = Vector(mother_x, mother_y)
+        local mother_x, mother_y = entity:get('SwarmMember').mothership:get('Physical').body:getPosition()
+        local mother_vector = Vector(mother_x, mother_y)
 
-        body = entity:get('Physical').body
-        member_x, member_y = body:getPosition()
-        member_vector = Vector(member_x, member_y)
-        relative = mother_vector - member_vector
-        direction = relative:normalize()
+        local body = entity:get('Physical').body
+        local member_x, member_y = body:getPosition()
+        local member_vector = Vector(member_x, member_y)
+        local relative = mother_vector - member_vector
+        local direction = relative:normalize()
 
         local force_factor = 2000
-        if relative:length() < 100 then
+        if relative:length() < 50 then
             body:applyForce(direction.x * -force_factor, direction.y * -force_factor)
-        else
+        elseif relative:length() > 150 then
             body:applyForce(direction.x * force_factor, direction.y * force_factor)
         end
+
+        local rotation_force_factor = 250
+        local direction_rotated = direction:rotate(math.rad(90))
+        body:applyForce(direction_rotated.x * rotation_force_factor,
+                        direction_rotated.y * rotation_force_factor,
+                        member_x, member_y)
     end
 end
 
