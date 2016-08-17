ffd42a4ef3f8ac63d0295a3b98045a136fcba8f7
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-08-17 17:41:58 +0200

    added drawable

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,17 @@
+
+local lovetoys = require('lib.lovetoys.lovetoys')
+lovetoys.initialize({
+    globals = true,
+    debug = true
+})
+
+-- Graphic components
+require("components/graphic/Drawable")
+
+--Graphic systems
+DrawSystem = require("systems/graphic/DrawSystem")
+
+
 function love.load()
 	print('Load')
 end
