657aad2ebfdd5ac54fa1b21fefad35a9748a9d0a
Sven-Hendrik Haase <svenstaro@gmail.com>
2016-08-17 17:33:09 +0200

    Basic base stuff

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,44 @@
+class = require('lib/middleclass')
+Vector = require('lib/vector')
+shine = require('lib/shine')
+lt = require('lib/lovetoys/lovetoys')
+lt.initialize()
+
 function love.load()
-	print('Load')
+    engine = lt.Engine()
+    world = love.physics.newWorld(0, 0, false)
+    world:setCallbacks(beginContact, endContact)
+    eventmanager = lt.EventManager()
 end
 
 function love.update(dt)
-	print('Update')
+    engine:update(dt)
+    world:update(dt)
+    if love.keyboard.isDown("a") then
+        print('a')
+    end
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
 
 function love.draw()
-	print('Draw')
+    engine:draw()
+end
+
+-- function love.keypressed(key, isrepeat)
+--     eventmanager:fireEvent(KeyPressed(key, isrepeat))
+-- end
+--
+-- function love.mousepressed(x, y, button)
+--     eventmanager:fireEvent(MousePressed(x, y, button))
+-- end
+
+function beginContact(a, b, coll)
+end
+
+function endContact(a, b, coll)
 end
