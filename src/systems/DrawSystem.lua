2eca06e62e7788edc5f8ee02e369bec5a782c453
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-08-17 22:38:03 +0200

    bulelt stub

--- src/systems/DrawSystem.lua
+++ src/systems/DrawSystem.lua
@@ -4,9 +4,17 @@ function DrawSystem:draw()
     for index, entity in pairs(self.targets) do
         drawable = entity:get("Drawable")
         love.graphics.setColor(unpack(drawable.color))
-        entity_x, entity_y = entity:get('Physical').body:getPosition()
-        radius = entity:get('Physical').shape:getRadius()
+
+        local physical = entity:get('Physical')
+        entity_x, entity_y = physical.body:getPosition()
+        angle = physical.body:getAngle()
+        radius = physical.shape:getRadius()
         love.graphics.circle("fill", entity_x, entity_y, radius, 100)
+
+        dir = Vector.from_radians(angle)
+        love.graphics.setColor(255,255,255,255)
+        love.graphics.circle("fill", entity_x+20*dir.x, entity_y+20*dir.y, 5, 100)
+
     end
 end
 
