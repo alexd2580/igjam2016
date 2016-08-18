f5f16ee7400c09956778aa826a9cb2c92ef77b63
Rafael Epplee <raffomania@gmail.com>
2016-08-18 19:52:35 +0200

    Add c4 warheads

--- src/items.lua
+++ src/items.lua
@@ -2,13 +2,13 @@ local Shield, Weapon = Component.load({"Shield", "Weapon"})
 
 items = {
     shield = {
-        name = "Shield",
+        name = "Energy Shield",
         component = Shield,
         layer = 2,
         image = resources.images.core2
     },
     laser = {
-        name = "Laser",
+        name = "Cryo-Laser",
         component = function()
             return Weapon({
                 type = 'laser',
@@ -19,5 +19,18 @@ items = {
         end,
         layer = 3,
         image = resources.images.eyes3
+    },
+    missiles = {
+        name = "C4 Warheads",
+        component = function()
+            return Weapon({
+                type = 'missile',
+                damage = 10,
+                cooldown = 4,
+                hitChance = 1
+            })
+        end,
+        layer = 3,
+        image = resources.images.eyes2
     }
 }
