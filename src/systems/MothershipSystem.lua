bd02478505a3f8437147bbb1b822dc84003e778e
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-08-18 17:41:05 +0200

    hard edges for motherships

--- src/systems/MothershipSystem.lua
+++ src/systems/MothershipSystem.lua
@@ -6,8 +6,34 @@ function MothershipSystem:update()
         --enemy_x, enemy_y = enemy_mothership:get("Physical").body:getPosition()
         --enemy_pos = Vector(enemy_x, enemy_y)
 
-        --body = entity:get('Physical').body
-        --member_x, member_y = body:getPosition()
+        local body = entity:get('Physical').body
+        local x, y = body:getPosition()
+        --local window_w, window_h,_ = love.window:getMode()
+        local window_w, window_h = 512, 448
+        local vel_x, vel_y = body:getLinearVelocity()
+        local imp_x, imp_y = 0,0
+
+        local hard_margin = 20
+        local soft_margin = 60
+
+        if x < hard_margin then x = hard_margin end
+        if x > window_w - hard_margin then x = window_w - hard_margin end
+        if y < hard_margin then y = hard_margin end
+        if y > window_h - hard_margin then y = window_h - hard_margin end
+
+        --if x < soft_margin then
+--            if vel_x < 0 then imp_x = 150-2.5*x/100 end
+--        end
+
+        --elseif member_x > window_w - margin then
+    --        vel_x = math.min(0, vel_y)
+    --    end
+
+        body:setPosition(x, y)
+--        print(imp_x, vel_x)
+--        body:applyLinearImpulse(-imp_x*vel_x, -imp_y*vel_y)
+
+
         --member_pos = Vector(member_x, member_y)
         --relative = enemy_pos - member_pos
         --direction_rad = relative:getRadian()
